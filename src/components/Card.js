0c5301c442c906b0a5ea5357cb71e580d02e6678
GauravPanchal <84240034+gauravhpanchal@users.noreply.github.com>
2024-08-17 21:11:55 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Card = ({ title, href, img }) => {
   return (
-    <Link to={href} className="relative">
+    <Link to={href} className="relative rounded-xl hover:ring-4 hover:ring-slate-200">
       <img src={img} alt="" className="size-[450px] object-cover rounded-xl" />
       <h1 className="absolute inset-0 top-8 flex justify-center text-3xl font-bold text-blue-500">
         {title}
